1785f4b6900ed87e955233a7aa561cf8be6e237a
Perla Nagarjuna Reddy <115425980+NagarjunaReddy43@users.noreply.github.com>
2023-02-27 10:27:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
 
-function palindrome(str){
+function palindrome(string){
+	const arrayValues = string.split('');
 
+    // reverse the array values
+    const reverseArrayValues = arrayValues.reverse();
+
+    // convert array to string
+    const reverseString = reverseArrayValues.join('');
+
+    if(string == reverseString) {
+        return true;
+    }
+    return false;
 }
 module.exports = palindrome
